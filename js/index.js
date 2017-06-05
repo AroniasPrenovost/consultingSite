5587a3516a1b4b84d8652e90de261d9cfe8798cd
AroniasPrenovost <Aroniascoder@gmail.com>
2017-06-04 22:51:49 -0700

    added styling to contact page, edited homepage

--- js/index.js
+++ js/index.js
@@ -2,18 +2,38 @@
 function chbg(color) {
 	document.getElementById('b').style.backgroundColor = color;
 }
+
 // quote generator
-var quotesArr = [];
-quotesArr[0] = '<span style="font-size:3.3em">We find Compatibility.</span> <br> <span style="font-size:1.0em">I asked Chris if he could make hair after finishing and he was happy to oblige.  He truly takes his time to make sure everything is perfect.</span>';
-quotesArr[1] = '<span style="font-size:3.3em">We help clients.</span> <br> <span style="font-size:1.0em">I asked Chris if he could make hair after finishing and he was happy to oblige.  He truly takes his time to make sure everything is perfect.</span>';
-quotesArr[2] = '<span style="font-size:3.3em">We understand goals.</span> <br> <span style="font-size:1.0em">I asked Chris if he could make hair after finishing and he was happy to oblige.  He truly takes his time to make sure everything is perfect.</span>';
+var quotesArr1 = [];
+quotesArr1[0] = '<span style="font-size:3.3em">We find Compatibility.</span> <br> <span style="font-size:1.0em; color:white">I asked Chris if he could make hair after finishing and he was happy to oblige.  He truly takes his time to make sure everything is perfect.</span>';
+
 // cycle through the quotes
 // setInterval(displayDetails, 3500);  // now we have buttons 
-function displayDetails(){
-	var randomQuote = Math.floor(Math.random() * (quotesArr.length - 0));
-	document.getElementById("quote").innerHTML = quotesArr[randomQuote];
+function displayDetails1(){
+	var randomQuote1 = Math.floor(Math.random() * (quotesArr1.length - 0));
+	document.getElementById("quote").innerHTML = quotesArr1[randomQuote1];
 	};
-displayDetails(); // sort of hacky but it's only a couple, right? 
-	document.getElementById("displayDetails").onclick = displayDetails;
-	document.getElementById("displayDetail").onclick = displayDetails;
-	document.getElementById("displayDetai").onclick = displayDetails;
+displayDetails1(); // sort of hacky but it's only a couple, right? 
+	document.getElementById("displayDetails1").onclick = displayDetails1;
+
+var quotesArr2 = [];
+quotesArr2[0] = '<span style="font-size:3.3em">We help clients.</span> <br> <span style="font-size:1.0em; color:white">I asked Chris if he could make hair after finishing and he was happy to oblige.  He truly takes his time to make sure everything is perfect.</span>';
+// cycle through the quotes
+// setInterval(displayDetails, 3500);  // now we have buttons 
+function displayDetails2(){
+	var randomQuote2 = Math.floor(Math.random() * (quotesArr2.length - 0));
+	document.getElementById("quote").innerHTML = quotesArr2[randomQuote2];
+	};
+displayDetails2(); // sort of hacky but it's only a couple, right? 
+	document.getElementById("displayDetails2").onclick = displayDetails2;
+
+var quotesArr3 = [];
+quotesArr3[0] = '<span style="font-size:3.3em">We understand goals.</span> <br> <span style="font-size:1.0em; color:white">I asked Chris if he could make hair after finishing and he was happy to oblige.  He truly takes his time to make sure everything is perfect.</span>';
+// cycle through the quotes
+// setInterval(displayDetails, 3500);  // now we have buttons 
+function displayDetails3(){
+	var randomQuote3 = Math.floor(Math.random() * (quotesArr3.length - 0));
+	document.getElementById("quote").innerHTML = quotesArr3[randomQuote3];
+	};
+displayDetails3(); // sort of hacky but it's only a couple, right? 
+	document.getElementById("displayDetails3").onclick = displayDetails3;
--- js/jquery.nav.js
+++ js/jquery.nav.js
@@ -19,7 +19,7 @@
 
 ;(function($, window, document, undefined){
 
-// jquery navbar brand disappear on scroll effect 
+	// jquery navbar brand disappear on scroll effect 
 $(window).scroll(function() {
 	if ($(this).scrollTop() > 0 && $(window).width()>768)
 	{
@@ -37,6 +37,47 @@ $(window).scroll(function() {
 	}
 });
 
+$( document ).ready( function() {
+	//Google Maps JS
+	//Set Map
+	function initialize() {
+			var myLatlng = new google.maps.LatLng(53.3333,-3.08333);
+			var mapOptions = {
+				zoom: 11,
+				center: myLatlng,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			}
+
+		var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+		//Callout Content
+		var contentString = 'Some address here..';
+		//Set window width + content
+		var infowindow = new google.maps.InfoWindow({
+			content: contentString,
+			maxWidth: 500
+		});
+
+		//Add Marker
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map
+		});
+
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(map,marker);
+		});
+
+		//Resize Function
+		google.maps.event.addDomListener(window, "resize", function() {
+			var center = map.getCenter();
+			google.maps.event.trigger(map, "resize");
+			map.setCenter(center);
+		});
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+
+});
 	// our plugin constructor
 	var OnePageNav = function(elem, options){
 		this.elem = elem;
